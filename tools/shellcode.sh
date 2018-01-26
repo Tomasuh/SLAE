77d630ae65779f4b78b067b041c9feb0ac65936a
axel.em.olsson@gmail.com <axel.em.olsson@gmail.com>
2018-01-26 03:26:13 +0100

    updated

--- tools/shellcode.sh
+++ tools/shellcode.sh
@@ -1,1 +1,2 @@
-for i in $(objdump -d $1 -M intel |grep "^ " |cut -f2); do echo -n '\x'$i; done;echo
+objdump -D $1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-7 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+
